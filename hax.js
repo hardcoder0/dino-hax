79a6da15cb066eaab036a144439b398bc39abc97
hardcoder0 <pwallitsch@outlook.com>
2021-12-11 11:11:28 -0800

    first commit

---
+++ hax.js
@@ -0,0 +1,1 @@
+if(confirm("Hax?")){let speed=prompt("Dino speed?", "10");let invincible=confirm("Invincible?");speed=isNaN(speed) ?  speed : 10;let cmd=`javascript: ${invincible ? 'Runner.instance_.gameOver=()=>{}' : ''};Runner.config.ACCELERATION=${speed} ;Runner.config.SPEED=${speed};Runner.config.MAX_SPEED=${speed}`;window.location.href = cmd;}
